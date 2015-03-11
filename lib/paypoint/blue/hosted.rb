e434ab97deb57a68bc987e2f99896d2f20f07926
Laszlo Bacsi <lackac@lackac.hu>
2015-03-11 23:45:09 +0100

    Make payment

--- lib/paypoint/blue/hosted.rb
+++ lib/paypoint/blue/hosted.rb
@@ -17,4 +17,19 @@ class PayPoint::Blue::Hosted < PayPoint::Blue::Base
     false
   end
 
+  # Make a payment
+  #
+  # All arguments will be merged into the final payload. For details on
+  # what to include, see https://developer.paypoint.com/payments/docs/#payments/make_a_payment
+  #
+  # @param [Hash] transaction details of the transaction you want to create
+  # @param [Hash] customer identity and details about the customer
+  # @param [Hash] session returnUrl, callbacks and skin
+  #
+  # @return [Hash] the API response
+  def make_payment(transaction:, customer:, session:, **options)
+    payload = options.merge transaction: transaction, customer: customer, session: session
+    client.post "sessions/#{inst_id}/payments", payload
+  end
+
 end
--- test/test_paypoint_blue_hosted.rb
+++ test/test_paypoint_blue_hosted.rb
@@ -10,4 +10,30 @@ class TestPayPointBlueHosted < Minitest::Test
     response = @blue.ping
     assert_equal true, response
   end
+
+  def test_make_payment
+    payload = {
+      transaction: {
+        merchantReference: "abcd-1234",
+        money: { amount: { fixed: "4.89" }, currency: "GBP" }
+      },
+      customer: {
+        identity: { merchantCustomerId: "42" },
+        details: { name: "John Doe" }
+      },
+      session: {
+        returnUrl: { url: "http://example.com/callback/abcd-1234" },
+        skin: "9001"
+      },
+      locale: "en"
+    }
+    stub_hosted_post('sessions/1234/payments').with(body: payload).to_return(fixture("make_payment.json"))
+    response = @blue.make_payment(**payload)
+    expected = {
+      "sessionId" => "39b3e3ec-92f4-48c4-aac8-c6c8bc9f6627",
+      "redirectUrl" => "https://hosted.mite.paypoint.net/hosted/4d9d53b5-06fc-41bb-91c6-a30e81175ed0/begin/39b3e3ec-92f4-48c4-aac8-c6c8bc9f6627",
+      "status" => "SUCCESS"
+    }
+    assert_equal expected, response
+  end
 end
